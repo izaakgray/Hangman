6ce0291085fc00340aa5195659a5632994a9a79f
Izaak Gray <u1864197@hud.ac.uk>
2018-09-28 19:58:39 +0100

    Basic functionality for guessing letters and removing lives

--- hangman.py
+++ hangman.py
@@ -6,12 +6,25 @@ word = random.choice(wordList)
 
 print('A word has been selected, please guess a letter!')
 
-guessedLetter = str(input())
+letters = len(word)
+lives = 10
 
-if len(guessedLetter) == 1:
-    print('Do code')
-else:
-    print('Please only enter a single letter.')
-    guessedLetter = str(input())
+print('Letters: ', letters)
+print('Lives remaining', lives)
+
+while lives != 0 and letters != 0:
+    guess = str(input('Guess: '))
+    if len(guess) == 1:
+        print('Do something')
+
+    else:
+        print('Please only enter a single letter.')
 
 
+    if guess in word:
+        print('Correct guess!')
+        letters = letters - 1
+
+    elif guess not in word:
+        lives = lives - 1
+        print('Incorrect guess. You have ', lives,' lives remaining.')
